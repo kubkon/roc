3565a21f379f6744d6f3ca975e8dc0f656f15dab
Jakub Konka <kubkon@jakubkonka.com>
2025-01-01 22:52:39 +0100

    linker/util: make is_roc_* helpers accept generic ObjectSymbol

--- crates/linker/src/util.rs
+++ crates/linker/src/util.rs
@@ -6,7 +6,7 @@ pub(crate) fn report_timing(label: &str, duration: Duration) {
     println!("\t{:9.3} ms   {}", duration.as_secs_f64() * 1000.0, label,);
 }
 
-fn is_roc_symbol(sym: &object::Symbol) -> bool {
+fn is_roc_symbol<'a>(sym: &impl ObjectSymbol<'a>) -> bool {
     if let Ok(name) = sym.name() {
         name.trim_start_matches('_').starts_with("roc_")
     } else {
@@ -14,10 +14,10 @@ fn is_roc_symbol(sym: &object::Symbol) -> bool {
     }
 }
 
-pub(crate) fn is_roc_definition(sym: &object::Symbol) -> bool {
+pub(crate) fn is_roc_definition<'a>(sym: &impl ObjectSymbol<'a>) -> bool {
     sym.is_definition() && is_roc_symbol(sym)
 }
 
-pub(crate) fn is_roc_undefined(sym: &object::Symbol) -> bool {
+pub(crate) fn is_roc_undefined<'a>(sym: &impl ObjectSymbol<'a>) -> bool {
     sym.is_undefined() && is_roc_symbol(sym)
 }
